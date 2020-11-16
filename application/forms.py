e5dd347ae09955e851026afa84cf22d732f9b6ec
nathanforester <nfeugene86@gmail.com>
2020-11-16 15:26:41 +0000

    debugging issues in progress

--- application/forms.py
+++ application/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired, ValidationError
 
-from application.models import Players, Review
+from application.models import Movies, Review
 
 class MoviesCheck: #customise
     def __init__(self, message):
