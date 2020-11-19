33034b070fbfd452fc1e9aadaf0695d6ca6eb133
nathanforester <nfeugene86@gmail.com>
2020-11-19 19:26:49 +0000

    testing .gitignore

--- application/__init__.py
+++ application/__init__.py
@@ -4,8 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:rootpassword123@34.89.101.184/footballdb'
-app.config['SECRET_KEY'] = '123456789'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@35.189.100.49/flask_db'
+#app.config['SECRET_KEY'] = '123456789'
 
 db = SQLAlchemy(app)
 
