6b2b60ccc6d7130e3d891b0d644f258edd367283
nathanforester1 <nathanforester32@gmail.com>
2020-12-15 00:29:02 +0000

    added startup file

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 
 cd /opt/movie_database
 sudo mkdir /flask_project
-sudo chown -r /flask_project
+sudo chown -R /flask_project
 sudo python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
